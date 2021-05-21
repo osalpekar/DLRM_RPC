ee07b6e5373ad22979235eb8fcdb3c0557233819
Omkar Salpekar <osalpekar@gmail.com>
2021-05-21 23:35:38 +0000

    correct interface name for multinode training

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
-GLOO_SOCKET_IFNAME=ens3 \
-TP_SOCKET_IFNAME=ens3 \
+GLOO_SOCKET_IFNAME=ens5 \
+TP_SOCKET_IFNAME=ens5 \
 python driver.py \
     --use-gpu \
     --num-nodes 2 \
     --node-rank $1 \
     --num-trainers 4 \
-    --master-addr q2-dy-p38xlarge-1 \
+    --master-addr q3-dy-p3dn24xlarge-4 \
     &> "out$1"
